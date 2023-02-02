bd53b4afa8bbff4e89a3426b0fe5134da3fb053e
Sandra Petereit <sandrap3393@googlemail.com>
2023-02-02 14:36:17 +0100

    implement add todo feature

--- todo-app/script.js
+++ todo-app/script.js
@@ -2,28 +2,78 @@ const input = document.querySelector("#todo-input");
 const addBtn = document.querySelector(".add-button");
 const todoList = document.querySelector(".todolist");
 
+const state = {
+  todos: [],
+  filter: "all",
+};
+
 /*****************************************************************************************************/
 
+addBtn.addEventListener("click", addTodo);
+
 loadTodos();
+
+/*****************************************************************************************************/
 
 function loadTodos() {
   fetch("http://localhost:4730/todos")
     .then((response) => response.json())
-    .then((data) => {
-      //console.log(data);
-      render(data);
+    .then((todosFromAPI) => {
+      state.todos = todosFromAPI;
+      render();
     });
 }
 
-function render(todoData) {
-  todoList.innerHTML = "";
+function addTodo() {
+  if (isInputValid(input.value)) {
+    const newTodo = { description: input.value, done: false };
 
-  for (let todo of todoData) {
-    createTodolistMarkup(todo);
+    fetch("http://localhost:4730/todos", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newTodo),
+    })
+      .then((response) => response.json())
+      .then((newTodoFromAPI) => {
+        state.todos.push(newTodoFromAPI);
+        createTodoItemMarkup(newTodoFromAPI);
+      });
   }
 }
 
-function createTodolistMarkup(todo) {
+/* 
+function removeTodo(id) {
+
+  fetch(`http://localhost:4730/todos/${id}`, {
+    method: "DELETE",
+  })
+    .then((response) => response.json())
+    .then(() => {
+      render();
+    });
+}
+
+
+function editTodo(id) {
+  fetch(`http://localhost:4730/todos/${id}`, {
+    method: "PUT",
+    headers: {'Content-Type': 'application/json'},
+  })
+  .then((response) => response.json())
+  .then((editedTodoFromAPI) => {
+  })
+
+}
+
+ */
+
+function render() {
+  todoList.innerHTML = "";
+
+  state.todos.forEach((todo) => createTodoItemMarkup(todo));
+}
+
+function createTodoItemMarkup(todo) {
   const listItem = document.createElement("li");
 
   const checkbox = document.createElement("input");
@@ -37,4 +87,10 @@ function createTodolistMarkup(todo) {
 
   listItem.append(checkbox, todoDescriptionLabel);
   todoList.appendChild(listItem);
+}
+
+function isInputValid(inputValue) {
+  if (inputValue.length >= 3) {
+    return true;
+  }
 }
