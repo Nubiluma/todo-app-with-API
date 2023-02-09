88dcdf20b47a2e1f279fbc1ab4a99dfbd470e5d6
Sandra Petereit <sandrap3393@googlemail.com>
2023-02-09 16:06:08 +0100

    set router database file to db-original.json

--- todo-api/server.js
+++ todo-api/server.js
@@ -2,7 +2,7 @@
 
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router(__dirname + "/db.json");
+const router = jsonServer.router(__dirname + "/db-original.json");
 const middlewares = jsonServer.defaults({});
 
 server.use(middlewares);
